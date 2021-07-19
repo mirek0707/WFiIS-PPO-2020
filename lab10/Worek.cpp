4853c9c80c8fdca007196d874661ab6a87f38d0a
mirek0707 <80774381+mirek0707@users.noreply.github.com>
2021-07-19 15:12:32 +0200

    Update Worek.cpp

--- lab10/Worek.cpp
+++ lab10/Worek.cpp
@@ -1,4 +1,31 @@
 #include "Worek.h"
+Worek::Worek(const Worek &v)
+{
+  Prezent * p1 = 0;
+  Prezent * p2 = 0;
+
+  if( v._head == 0 )
+  {
+    _head = 0;
+  }
+  else
+  {
+    _head = new Prezent();
+    _head -> _wartosc = v._head -> _wartosc;
+
+    p1 = _head;
+    p2 = v._head -> next;
+  }
+  while( p2 )
+  {
+    p1 -> next = new Prezent();
+    p1 = p1 -> next;
+    p1 -> _wartosc = p2 -> _wartosc;
+
+    p2 = p2 -> next;
+  }
+  p1 -> next = 0;
+}
 void Worek::Wloz(Prezent pr)
 {
   
